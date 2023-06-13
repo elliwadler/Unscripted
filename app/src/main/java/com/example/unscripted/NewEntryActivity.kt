66dc323ca8a30509d7fc84f40676ed1528cc5dde
Elisabeth <elisabeth.wadler@edu.fh-joanneum.at>
2023-06-13 17:06:32 +0200

    sv

--- app/src/main/java/com/example/unscripted/NewEntryActivity.kt
+++ app/src/main/java/com/example/unscripted/NewEntryActivity.kt
@@ -1,38 +1,77 @@
 package com.example.unscripted
 
-import android.content.Context
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.activity.result.ActivityResultLauncher
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
-import com.google.gson.Gson
-import java.text.SimpleDateFormat
-import java.util.Locale
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-class NewEntryActivity : AppCompatActivity() {
+
+class NewEntryActivity : BasisActivity() {
+    private val CAMERA_PERMISSION_REQUEST_CODE = 100
+    private val GALLERY_PERMISSION_REQUEST_CODE = 101
+
+    private var selectedImage: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_entry)
 
-        val btnSelectImage: Button = findViewById(R.id.btn_new_addImage)
+        val pickImage: Button = findViewById(R.id.btn_new_addImage)
+        selectedImage = findViewById(R.id.img_picked)
 
-        btnSelectImage.setOnClickListener {
-            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            filePhoto = getPhotoFile(FILE_NAME)
-            val providerFile =
-                FileProvider.getUriForFile(this,"com.example.androidcamera.fileprovider", filePhoto)
-            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
-
+        pickImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // Permission already granted, start image selection
+                pickImageFromGallery()
+            } else {
+                // Request permission
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(READ_EXTERNAL_STORAGE),
+                    GALLERY_PERMISSION_REQUEST_CODE
+                )
+            }
         }
-
-
     }
 
+    private fun pickImageFromGallery() {
+        val pickImg = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        changeImage.launch(pickImg)
+    }
+
+    private val changeImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            val imgUri = data?.data
+
+            selectedImage?.setImageURI(imgUri)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == GALLERY_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start image selection
+                pickImageFromGallery()
+            } else {
+                // Permission denied
+                showCustomSnackbar(getString(R.string.storage_permission_denied), true)
+            }
+        }
+    }
 }
+
+
+
+
